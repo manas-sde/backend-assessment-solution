1f0b7e6250afa244dc91e9f55104a9caccd4f064
manas-dev <omishra717@gmail.com>
2023-04-19 19:19:31 +0530

    CosmoCloud Backend Developer Solution

--- main.py
+++ main.py
@@ -28,5 +28,29 @@ def merge_lists(list_1, list_2) -> list:
     """
     # return list_3
 
+    #--------- Solution--------------
+
+    '''
+    Approach - 
+        - Declare a empty dictionary with {} as default value for keys(say: res_dict)
+        - Iterate over each element of both list 
+        - Add each list element to res_dict with key as value of id attribute of that element
+        - Finally, return all the values as a list of res_dict dictionary
+    
+    Time Complexity - O(n1+n2) ; n1-> length of list_1, n2-> length of list_2
+    '''
+
+    from collections import defaultdict as dd
+
+    res_dict = dd(lambda:{})
+    
+    for data_obj in list_1+list_2:
+        if "id" in data_obj.keys():
+                res_dict[data_obj["id"]].update(data_obj)
+
+    list_3 =  list(res_dict.values())
+
+    return list_3
+
 
 list_3 = merge_lists(list_1, list_2)
